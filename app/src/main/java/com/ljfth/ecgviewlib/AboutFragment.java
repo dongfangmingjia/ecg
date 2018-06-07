291c6ded7ead6ced1eb3f6d8e64eff5dc591e70f
wangdongjian <wangdongjian@suiyueyule.com>
2018-06-07 21:30:41 +0800

    主题、关于

---
+++ app/src/main/java/com/ljfth/ecgviewlib/AboutFragment.java
@@ -0,0 +1,25 @@
+package com.ljfth.ecgviewlib;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ljfth.ecgviewlib.base.BaseFragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends BaseFragment {
+
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.fragment_about;
+    }
+
+
+}
--- app/src/main/java/com/ljfth/ecgviewlib/base/BaseActivity.java
+++ app/src/main/java/com/ljfth/ecgviewlib/base/BaseActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import java.util.List;
 
